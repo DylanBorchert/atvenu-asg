2086bf827e4b4aa872d2eefb205fb607e3a7ad5e
Dylan Borchert <dylanbor01@gmail.com>
2024-05-22 18:38:50 -0600

    chore: fix poster names

--- src/data/posters.js
+++ src/data/posters.js
@@ -3,12 +3,12 @@ const posters = `[
         "id": 1,
         "artist": "ACDC",
         "title": "Highway to Hell",
+        "filename": "ACDC - Highway to Hell - Rock Poster.jpg",
         "description": "Iconic rock poster featuring ACDC, perfect for collectors and fans.",
-        "dimensions": [
+        "options": [
             {
 				"id": 1,
-                "dimensions": "24 x 36",
-                "filename": "ACDC - Highway to Hell - Rock Poster (24 x 36 inches).jpg",
+                "dimensions": "24x36",
                 "CountIn": 100,
                 "Add": 20,
                 "Comp": 10,
@@ -21,12 +21,12 @@ const posters = `[
         "id": 2,
         "artist": "Led Zeppelin",
         "title": "Celebration Day",
+        "filename": "Led Zeppelin - Celebration Day - Rock Poster.jpg",
         "description": "A vintage-style poster of Led Zeppelin’s Celebration Day.",
-        "dimensions": [
+        "options": [
             {
 				"id": 1,
-                "dimensions": "24 x 36",
-                "filename": "Led Zeppelin - Celebration Day - Rock Poster (24 x 36 inches).jpg",
+                "dimensions": "24x36",
                 "CountIn": 100,
                 "Add": 15,
                 "Comp": 5,
@@ -39,12 +39,12 @@ const posters = `[
         "id": 3,
         "artist": "Nirvana",
         "title": "Nevermind",
+        "filename": "Nirvana - Nevermind - Australian Tour Poster.jpg",
         "description": "Commemorative tour poster for Nirvana's legendary Australian tour.",
-        "dimensions": [
+        "options": [
             {
 				"id": 1,
-                "dimensions": "24 x 36",
-                "filename": "Nirvana - Nevermind - Australian Tour Poster (24 x 36 inches).jpg",
+                "dimensions": "24x36",
                 "CountIn": 150,
                 "Add": 25,
                 "Comp": 15,
@@ -57,12 +57,12 @@ const posters = `[
         "id": 4,
         "artist": "Nirvana",
         "title": "Smiley Face",
+        "filename": "Nirvana - Smiley Face - Rock Poster.jpg",
         "description": "Iconic Nirvana Smiley Face poster, a must-have for grunge fans.",
-        "dimensions": [
+        "options": [
             {
 				"id": 1,
-                "dimensions": "24 x 36",
-                "filename": "Nirvana - Smiley Face - Rock Poster (24 x 36 inches).jpg",
+                "dimensions": "24x36",
                 "CountIn": 120,
                 "Add": 30,
                 "Comp": 12,
@@ -75,12 +75,12 @@ const posters = `[
         "id": 5,
         "artist": "Pink Floyd",
         "title": "Radio City 1973",
+        "filename": "Pink Floyd - Radio City 1973 - Rock Poster.jpg",
         "description": "Celebrate Pink Floyd's historic Radio City 1973 performance with this poster.",
-        "dimensions": [
+        "options": [
             {
 				"id": 1,
-                "dimensions": "24 x 36",
-                "filename": "Pink Floyd - Radio City 1973 - Rock Poster (24 x 36 inches).jpg",
+                "dimensions": "24x36",
                 "CountIn": 90,
                 "Add": 10,
                 "Comp": 8,
@@ -93,12 +93,12 @@ const posters = `[
         "id": 6,
         "artist": "Pink Floyd",
         "title": "Tour Poster by Bob Masse",
+        "filename": "Pink Floyd - Tour Poster by Bob Masse.jpg",
         "description": "Artistic poster by Bob Masse for a Pink Floyd tour, with vivid artwork.",
-        "dimensions": [
+        "options": [
             {
 				"id": 1,
-                "dimensions": "24 x 36",
-                "filename": "Pink Floyd - Tour Poster by Bob Masse (24 x 36 inches).jpg",
+                "dimensions": "24x36",
                 "CountIn": 80,
                 "Add": 20,
                 "Comp": 5,
@@ -111,12 +111,12 @@ const posters = `[
         "id": 7,
         "artist": "Radiohead",
         "title": "Ok Computer",
+        "filename": "Radiohead - Ok Computer - Poster.jpg",
         "description": "A minimalist poster reflecting the ethos of Radiohead’s Ok Computer.",
-        "dimensions": [
+        "options": [
             {
 				"id": 1,
-                "dimensions": "24 x 36",
-                "filename": "Radiohead - Ok Computer - Poster (24 x 36 inches).jpg",
+                "dimensions": "24x36",
                 "CountIn": 110,
                 "Add": 15,
                 "Comp": 11,
@@ -129,12 +129,12 @@ const posters = `[
         "id": 8,
         "artist": "Tame Impala",
         "title": "Currents",
+        "filename": "Tame Impala - Currents - Rock Poster.jpg",
         "description": "Poster featuring Tame Impala's album Currents, available in two sizes.",
-        "dimensions": [
+        "options": [
             {
 				"id": 1,
-                "dimensions": "16 x 24",
-                "filename": "Tame Impala - Currents - Rock Poster (16 x 24 inches).jpg",
+                "dimensions": "16x24",
                 "CountIn": 70,
                 "Add": 10,
                 "Comp": 7,
@@ -143,8 +143,7 @@ const posters = `[
             },
             {
 				"id": 2,
-                "dimensions": "24 x 36",
-                "filename": "Tame Impala - Currents - Rock Poster (24 x 36 inches).jpg",
+                "dimensions": "24x36",
                 "CountIn": 120,
                 "Add": 25,
                 "Comp": 10,
@@ -157,12 +156,12 @@ const posters = `[
         "id": 9,
         "artist": "The Police",
         "title": "Reggatta de Blanc",
+        "filename": "The Police - Reggatta de Blanc - Rock Poster.jpg",
         "description": "Iconic album cover poster from The Police’s Reggatta de Blanc.",
-        "dimensions": [
+        "options": [
             {
 				"id": 1,
-                "dimensions": "24 x 36",
-                "filename": "The Police - Reggatta de Blanc - Rock Poster (24 x 36 inches).jpg",
+                "dimensions": "24x36",
                 "CountIn": 85,
                 "Add": 20,
                 "Comp": 9,
